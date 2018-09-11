45e0e806330fe1260f340eb9ea0639f7aba6d942
mhaagens <martin.haagensli@gmail.com>
2018-09-11 08:25:09 +0200

    initial commit

---
+++ babel/index.js
@@ -0,0 +1,41 @@
+import { declare } from "@babel/helper-plugin-utils";
+
+import presetReact from "@babel/preset-react";
+import transformRegenerator from "@babel/plugin-transform-regenerator";
+import transformRuntime from "@babel/plugin-syntax-dynamic-import";
+import syntaxDynamicImport from "@babel/plugin-syntax-dynamic-import";
+import classPropertiesProposal from "@babel/plugin-proposal-class-properties";
+import decoratorsProposal from "@babel/plugin-proposal-decorators";
+import restSpreadProposal from "@babel/plugin-proposal-object-rest-spread";
+import loadablePlugin from "react-loadable/babel";
+import hotLoaderPlugin from "react-hot-loader/babel";
+import styledPlugin from "babel-plugin-styled-components";
+
+export default declare((api, opts) => {
+  api.assertVersion(7);
+  const production = process.env.NODE_ENV === "production";
+
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          modules: false,
+          targets: ["> 1%", "last 2 versions", "not ie <= 8"]
+        }
+      ],
+      "@babel/preset-react"
+    ],
+    plugins: [
+      production && transformRenegerator,
+      production && transformRuntime,
+      syntaxDynamicImport,
+      classPropertiesProposal,
+      [decoratorsProposal, { legacy: true }],
+      restSpreadProposal,
+      loadablePlugin,
+      hotLoaderPlugin,
+      styledPlugin
+    ]
+  };
+});
