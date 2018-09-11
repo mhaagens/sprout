e92e25d328b06a94ac7c516476997bf733193812
Martin Haagensli <martin.haagensli@gmail.com>
2018-09-11 11:54:56 +0200

    working config

--- packages/babel-preset-sprout/lib/index.js
+++ packages/babel-preset-sprout/lib/index.js
@@ -1,16 +1,17 @@
-module.exports = (ops) => {
+module.exports = opts => {
   const production = process.env.NODE_ENV === "production";
-  const ssr = opts.ssr || false;
+  const ssr = opts.ssr || false;
+  const targets = opts.targets || {
+    node: "current",
+    browsers: ["> 1%", "last 2 versions", "not ie <= 8"]
+  };
   return {
     presets: [
       [
         require("@babel/preset-env"),
         {
           modules: false,
-          targets: {
-            node: "current",
-            browsers: ["> 1%", "last 2 versions", "not ie <= 8"]
-          }
+          targets
         }
       ],
       require("@babel/preset-react")
@@ -18,16 +19,18 @@ module.exports = (ops) => {
     plugins: [
       require("@babel/plugin-transform-regenerator"),
       require("@babel/plugin-syntax-dynamic-import"),
-      require("@babel/plugin-syntax-dynamic-import"),
       require("@babel/plugin-proposal-class-properties"),
       [require("@babel/plugin-proposal-decorators"), { legacy: true }],
       require("@babel/plugin-proposal-object-rest-spread"),
       require("react-loadable/babel"),
       require("react-hot-loader/babel"),
-      [require("babel-plugin-styled-components"), {
-        ssr: ssr,
-        displayName: !production
-      }]
+      [
+        require("babel-plugin-styled-components"),
+        {
+          ssr: ssr,
+          displayName: !production
+        }
+      ]
     ]
   };
 };
