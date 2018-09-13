91e1ac806d36f1dac414131ad19f844abc70bf66
Martin Haagensli <martin.haagensli@gmail.com>
2018-09-13 08:34:03 +0200

    will this work...

--- packages/babel-preset/src/index.js
+++ packages/babel-preset/src/index.js
@@ -1,25 +1,35 @@
-module.exports = opts => {
-    const production = opts.production || process.env.NODE_ENV === "production";
-    const ssr = opts.ssr || false;
+module.exports = function(opts) {
+  const production = opts.production || process.env.NODE_ENV === "production";
+  const ssr = opts.ssr || false;
 
-    return {
-      plugins: [
-        require("@babel/plugin-transform-runtime"),
-        require("@babel/plugin-transform-regenerator"),
-        require("@babel/plugin-syntax-dynamic-import"),
-        require("@babel/plugin-proposal-class-properties"),
-        [require("@babel/plugin-proposal-decorators"), { legacy: true }],
-        require("@babel/plugin-proposal-object-rest-spread"),
-        require("react-loadable/babel"),
-        require( "react-hot-loader/babel"),
-        [
-          require("babel-plugin-styled-components"),
-          {
-            ssr: ssr,
-            displayName: !production
-          }
-        ]
+  return {
+    plugins: [
+      require("@babel/plugin-transform-runtime"),
+      require("@babel/plugin-transform-regenerator"),
+      require("@babel/plugin-syntax-dynamic-import"),
+      require("@babel/plugin-proposal-class-properties"),
+      [require("@babel/plugin-proposal-decorators"), { legacy: true }],
+      require("@babel/plugin-proposal-object-rest-spread"),
+      require("react-loadable/babel"),
+      require("react-hot-loader/babel"),
+      [
+        require("babel-plugin-styled-components"),
+        {
+          ssr: ssr,
+          displayName: !production
+        }
       ]
-    };
+    ],
+    presets: [
+      [
+        require("@babel/preset-env"),
+        {
+          targets: {
+            browsers: ["> 1%", "last 2 versions", "not ie <= 8"]
+          }
+        }
+      ],
+      require("@babel/preset-react")
+    ]
   };
-  
+};
