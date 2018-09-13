ee38b18447e38d602db68dbfa42686fca0674fcc
Martin Haagensli <martin.haagensli@gmail.com>
2018-09-13 08:42:56 +0200

    targets

--- packages/babel-preset/src/index.js
+++ packages/babel-preset/src/index.js
@@ -1,12 +1,17 @@
 module.exports = function(opts) {
   const production = opts.production || process.env.NODE_ENV === "production";
   const ssr = opts.ssr || false;
+  const targets = opts.targets;
+
+  if (!opts.targets) {
+    throw new Error("babel-preset-sprout: Targets must be defined");
+  }
 
   return {
     plugins: [
+      require("@babel/plugin-syntax-dynamic-import"),
       require("@babel/plugin-transform-runtime"),
       require("@babel/plugin-transform-regenerator"),
-      require("@babel/plugin-syntax-dynamic-import"),
       require("@babel/plugin-proposal-class-properties"),
       [require("@babel/plugin-proposal-decorators"), { legacy: true }],
       require("@babel/plugin-proposal-object-rest-spread"),
@@ -24,9 +29,8 @@ module.exports = function(opts) {
       [
         require("@babel/preset-env"),
         {
-          targets: {
-            browsers: ["> 1%", "last 2 versions", "not ie <= 8"]
-          }
+          modules: false,
+          targets
         }
       ],
       require("@babel/preset-react")
